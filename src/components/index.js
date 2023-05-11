49c50a186ecd083b71fc64103eb4421a78ca633f
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-11 23:54:53 +0200

    About Us container implemented

--- src/components/index.js
+++ src/components/index.js
@@ -4,10 +4,4 @@ import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './Menuitem/MenuItem';
 import Navbar from './Navbar/Navbar';
 
-export {
-  SubHeading,
-  Newsletter,
-  FooterOverlay,
-  MenuItem,
-  Navbar,
-};
+export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 
 import './AboutUs.css';
+import images from '../../constants/images';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className='app__aboutus app__bg flex__center section__padding'
+    id='about'
+  >
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt='G Letter' />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content-about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img
+          src={images.spoon}
+          alt='aboutSpoon'
+          className='spoon__img spoon__img-reverse'
+        />
+        <p style={{ margin: '2rem 0' }} className='p__opensans'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className='custom__button'>Know More</button>
+      </div>
+      <div className='app__aboutus__knife flex__center'>
+        <img src={images.knife} alt='Knife' />
+      </div>
+      <div className='app__aboutus-content-history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='aboutSpoon' className='spoon__img' />
+        <p style={{ margin: '2rem 0' }} className='p__opensans'>
+          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+          odio nec aliquet.
+        </p>
+        <button className='custom__button'>Know More</button>
+      </div>
+    </div>
   </div>
 );
 
