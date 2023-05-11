6c1c211ed18587991d9ee819951077e27bd42a4b
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-11 23:57:14 +0200

    Header, Intro containers implemented

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
 import './Header.css';
+import images from '../../constants/images';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper-info'>
+      <SubHeading title='Chase the new Flavour' />
+      <h1 className='app__header-h1'>The key to Fine dining</h1>
+      <p style={{ margin: '2rem 0' }} className='p__opensans app__header__p'>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </p>
+      <button className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper__img'>
+      <img src={images.welcome} alt='HeaderImage' />
+    </div>
   </div>
 );
 
